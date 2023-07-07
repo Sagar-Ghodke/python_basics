f0c7ee59aea574fd4e31532b423f75984b2f35de
Sagar Ghodke <71428318+Sagar-Ghodke@users.noreply.github.com>
2023-07-07 16:42:21 +0530

    Delete python practice directory

--- python practice/practice1.py
+++
@@ -1,8 +0,0 @@
-
-fruits=[" MANGO ","CHERRY", "@#Bananas@!", "grapes"]
-
-def clean(x):
-    return x.strip(' @!%$#').capitalize()
-fruits=[" MANGO ","CHERRY", "@#Bananas@!", "grapes"]
-fruits=[clean(i) for i in fruits]
-print(fruits)
